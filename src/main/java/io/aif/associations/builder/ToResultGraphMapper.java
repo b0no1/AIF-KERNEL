a444ca60a1080be977aa0c7ef55d79e6a94bf1c7
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2015-03-08 18:08:01 +0000

    final version of connection calculation

--- src/main/java/io/aif/associations/builder/ToResultGraphMapper.java
+++ src/main/java/io/aif/associations/builder/ToResultGraphMapper.java
@@ -3,18 +3,42 @@ package io.aif.associations.builder;
 
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.SparseGraph;
+import io.aif.associations.calculators.vertex.IVertexWeightCalculator;
 import io.aif.associations.model.IAssociationEdge;
 import io.aif.associations.model.IAssociationVertex;
 
 import java.awt.geom.Arc2D;
+import java.util.HashMap;
+import java.util.Map;
 
-public class ToResultGraphMapper {
+public class ToResultGraphMapper<T> {
     
-    public static <T>Graph<IAssociationVertex<T>, IAssociationEdge> map(final Graph<T, Double> connections) {
+    private final IVertexWeightCalculator<T> vertexWeightCalculator;
+
+    public ToResultGraphMapper(IVertexWeightCalculator<T> vertexWeightCalculator) {
+        this.vertexWeightCalculator = vertexWeightCalculator;
+    }
+
+    public Graph<IAssociationVertex<T>, IAssociationEdge> map(final Graph<T, Double> connections) {
         final Graph<IAssociationVertex<T>, IAssociationEdge> resultGraph = new SparseGraph<>();
+        final Map<T, IAssociationVertex<T>> oldToNewVertexMapping = new HashMap<>();
         connections.getVertices().forEach(vertex -> {
-            // TODO
+            oldToNewVertexMapping.put(vertex, new AssociationVertex<>(vertex, vertexWeightCalculator.calculate(vertex)));
         });
+        connections.getVertices().forEach(vertex -> {
+            mapVertexConnections(oldToNewVertexMapping.get(vertex), oldToNewVertexMapping, resultGraph, connections);
+        });
+        return resultGraph;
+    }
+    
+    private void mapVertexConnections(final IAssociationVertex<T> current, 
+                                      final Map<T, IAssociationVertex<T>> oldToNewVertexMapping, 
+                                      final Graph<IAssociationVertex<T>, IAssociationEdge> resultGraph,
+                                      final Graph<T, Double> connections) {
+        connections.getNeighbors(current.item()).forEach(neighbor -> {
+            final IAssociationVertex<T> newNeighbor = oldToNewVertexMapping.get(neighbor);
+            resultGraph.addEdge(new AssociationEdge(connections.findEdge(current.item(), neighbor)), current, newNeighbor);
+        }); 
     }
     
 }
