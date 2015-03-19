e1e6e838b3d5cf0a5a98db2dd37691d2062d3f7e
Viacheslav Kovalevskyi <vsk@amazon.com>
2015-03-19 17:57:02 +0000

    adding initial spec2 tests

---
+++ src/test/scala/io/aif/associations/builder/QuickStartSpec.scala
@@ -0,0 +1,12 @@
+package io.aif.associations.builder
+
+import org.specs2._
+
+
+class QuickStartSpec extends Specification { def is = s2"""
+
+ This is my first specification
+   it is working                 $ok
+   really working!               $ok
+                                 """
+}
