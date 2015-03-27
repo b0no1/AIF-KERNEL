931e5c422b64b0a2a6da1e705a275d28384f190b
Viacheslav Kovalevskyi <vsk@amazon.com>
2015-03-27 18:39:00 +0000

    moving to new artifactory URL: artifactory.aif.io

---
+++ src/test/java/io/aif/associations/builder/ExperimentsConnectionsGraphReducerTest.java
@@ -0,0 +1,13 @@
+package io.aif.associations.builder;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExperimentsConnectionsGraphReducerTest {
+
+    @Test
+    public void testReduce() throws Exception {
+
+    }
+}
