81e5275cae4b2fc6723d6f6e9a33a591b427996c
Viacheslav Kovalevskyi <vsk@amazon.com>
2015-03-27 18:40:27 +0000

    adding test for empty map during reduce operaiton

--- src/test/java/io/aif/associations/builder/ExperimentsConnectionsGraphReducerTest.java
+++ src/test/java/io/aif/associations/builder/ExperimentsConnectionsGraphReducerTest.java
@@ -2,12 +2,19 @@ package io.aif.associations.builder;
 
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class ExperimentsConnectionsGraphReducerTest {
 
     @Test
-    public void testReduce() throws Exception {
+    public void testReduceQithEmptyMap() throws Exception {
+        final ExperimentsConnectionsGraphReducer<Integer> experimentsConnectionsGraphReducer = new ExperimentsConnectionsGraphReducer<>();
+        final Map<Integer, Map<Integer, Double>> actualResult = experimentsConnectionsGraphReducer.reduce(Collections.emptyMap());
 
+        assertTrue(actualResult.isEmpty());
     }
+
 }
