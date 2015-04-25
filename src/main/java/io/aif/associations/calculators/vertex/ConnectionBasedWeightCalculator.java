eb7d2fe0cb626a0f769c9365ee8a728669e08c99
Viacheslav Kovalevskyi <vsk@amazon.com>
2015-04-25 16:30:57 +0100

    quality improvmants

--- src/main/java/io/aif/associations/calculators/vertex/ConnectionBasedWeightCalculator.java
+++ src/main/java/io/aif/associations/calculators/vertex/ConnectionBasedWeightCalculator.java
@@ -8,7 +8,11 @@ public class ConnectionBasedWeightCalculator<T> implements IVertexWeightCalculat
 
     @Override
     public Map<T, Double> calculate(final Map<T, Map<T, Double>>  graph, final Map<T, Long> count) {
-        final Map<T, Double> weights = graph.keySet().stream().collect(Collectors.toMap(k -> k, k -> calculate(k, graph, count)));
+        final OptionalDouble optAverageConnectionsCount = graph.keySet().stream().mapToInt(connection -> graph.get(connection).keySet().size()).average();
+        if (!optAverageConnectionsCount.isPresent()) return Collections.emptyMap();
+        final Double averageConnectionsCount = optAverageConnectionsCount.getAsDouble();
+
+        final Map<T, Double> weights = graph.keySet().stream().collect(Collectors.toMap(k -> k, k -> calculate(k, graph, count, averageConnectionsCount)));
         final Optional<Double> optMax = weights.values().stream().max(Double::compare);
         if (!optMax.isPresent()) return Collections.emptyMap();
         if (optMax.get() == .0) {
@@ -17,7 +21,7 @@ public class ConnectionBasedWeightCalculator<T> implements IVertexWeightCalculat
         return weights.keySet().stream().collect(Collectors.toMap(k -> k, k -> weights.get(k) / optMax.get()));
     }
 
-    private double calculate(final T vertex, final Map<T, Map<T, Double>> graph, final Map<T, Long> count) {
+    private double calculate(final T vertex, final Map<T, Map<T, Double>> graph, final Map<T, Long> count, final Double averageConnectionsCount) {
         if (graph.get(vertex).keySet().isEmpty()) return .0;
 
         final OptionalDouble optAverageWeight = graph.get(vertex).keySet().stream()
@@ -25,7 +29,18 @@ public class ConnectionBasedWeightCalculator<T> implements IVertexWeightCalculat
                 .max();
         if (!optAverageWeight.isPresent())
             return .0;
-        return (optAverageWeight.getAsDouble()) / ((double)graph.get(vertex).keySet().size() * (double)count.get(vertex));
+
+//        final Double sum = graph.get(vertex).keySet().stream()
+//                .mapToDouble(neighbor -> graph.get(vertex).get(neighbor)).sum();
+//
+//        final Double newCount = (double)count.get(vertex) * (optAverageWeight.getAsDouble() / sum);
+//
+//        return (optAverageWeight.getAsDouble()) / (Math.abs((double)graph.get(vertex).keySet().size() - averageConnectionsCount) * newCount);
+//        if (vertex.toString().contains("[go]") || vertex.toString().contains("[Lots]")) {
+//            System.out.println("sfd");
+//        }
+        System.out.println(vertex + "; " + graph.get(vertex).keySet().size() + "; " + count.get(vertex));
+        return (optAverageWeight.getAsDouble()) / (Math.abs((double)graph.get(vertex).keySet().size() - averageConnectionsCount) * (double)count.get(vertex));
     }
     
 }
